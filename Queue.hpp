87675044e8a93cf0c54d4bc22d58e36fcaa5b506
Diogo Pinheiro <diogo.pinheiro1996@gmail.com>
2025-04-16 19:06:09 +0100

    git updates

---
+++ Queue.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <mutex>
+#include <iostream>
+#include <vector>
+#include <condition_variable>
+
+
+template<typename T>
+class Queue
+{
+	private:
+		T* _buffer;                // Dynamically allocated buffer
+		int _maxSize;              // Max elements in queue
+		int _head;                 // Index for popping
+		int _tail;                 // Index for pushing
+		int _count;                // Current element count
+
+		mutable std::mutex mtx;               // Mutex for thread safety
+		std::condition_variable cv;           // Condition variable for blocking pop
+		void advance(int& index);             // Helper for circular index
+
+	public:
+		Queue(int size);
+		~Queue();
+		//void Push(T element);
+		//T Pop();
+		//T PopWithTimeout(int milliseconds);
+		//int Count(); // Amount of elements stored now
+		//int Size(); // Max number of elements 
+};
+
---
+++ Queue.tpp
@@ -0,0 +1,7 @@
+#include "Queue.hpp"
+
+template<typename T>
+Queue<T>::Queue(int size)
+{
+
+}
