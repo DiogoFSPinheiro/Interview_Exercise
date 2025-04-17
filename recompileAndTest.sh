3245fe3817f462a66b6bab974174c45c9d5e4b34
Diogo Pinheiro <diogo.pinheiro1996@gmail.com>
2025-04-17 17:45:39 +0100

    Changed script to be more helpfull

--- recompileAndTest.sh
+++ recompileAndTest.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
-clear
 
-rm -rf build/
-mkdir build && cd build
-cmake ..
-make
 
-clear
-ctest
+if [ "$1" == "remake" ]; then
+    rm -rf build/
+    mkdir build && cd build
+    cmake ..
+    make
+    clear
+    ctest
+
+elif [ "$1" == "maketest" ]; then
+    cd build
+    make
+    ./queue_tests
+
+elif [ "$1" == "delete" ]; then
+	rm -rf build/
+	echo "Deleted Build!"
+
+else
+    echo "You forgot the order: remake, maketest or delete!"
+fi
+
+
